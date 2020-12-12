168cb31c848ebbcb67f359888bee1fb1808281cd
Utkrist Neupane <utkrist991@gmail.com>
2020-12-12 17:33:59 +0545

    Updated Repo

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-print('''asfasfasfahgiasnghyuijnbgyuhjnbgtyujjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjfdsf
-jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj''')
-lis = ['sadfsa', 'asfdasfas', 'fasfasfasf']
-for i in lis:
-    print(i , sep="\n")
+def a():
+    a = input("anyhting")
+    b = input("koags")
+    return a,b
+    
+def b(a,b):
+    print(a,b)
+
+def c():
+    aa , bb = a()
+    b(aa, bb)
+c()
